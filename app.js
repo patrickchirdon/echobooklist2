635c95ff37754d14188aa8f6b852c7cbeaae0b83
patrickchirdon <patrickchirdon@pop-os.localdomain>
2022-08-30 23:48:10 -0400

    make it better4

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ app.use("/books/:id/comments",commentRoute);
 app.use("/",indexRoute);
 
 
-app.listen(80, process.env.IP, function(){
+app.listen(3000, process.env.IP, function(){
     console.log("Server started");
 });
